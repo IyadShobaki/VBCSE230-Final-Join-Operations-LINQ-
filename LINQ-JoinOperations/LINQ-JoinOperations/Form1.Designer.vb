70a6e9ece93398800690eb34ffb2e3ec0bbdb6eb
Iyad Shobaki <iyadshobaki82@gmail.com>
2018-12-05 15:09:20 -0500

    first and final commit

---
+++ LINQ-JoinOperations/LINQ-JoinOperations/Form1.Designer.vb
@@ -0,0 +1,156 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnJoin2 = New System.Windows.Forms.Button()
+        Me.btnGroupJoin = New System.Windows.Forms.Button()
+        Me.btnJoinmultiple = New System.Windows.Forms.Button()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.DataGridView3 = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 74)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(372, 364)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'btnJoin2
+        '
+        Me.btnJoin2.Font = New System.Drawing.Font("Georgia", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnJoin2.Location = New System.Drawing.Point(58, 28)
+        Me.btnJoin2.Name = "btnJoin2"
+        Me.btnJoin2.Size = New System.Drawing.Size(251, 40)
+        Me.btnJoin2.TabIndex = 1
+        Me.btnJoin2.Text = "Join two tables"
+        Me.btnJoin2.UseVisualStyleBackColor = True
+        '
+        'btnGroupJoin
+        '
+        Me.btnGroupJoin.Font = New System.Drawing.Font("Georgia", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnGroupJoin.Location = New System.Drawing.Point(448, 27)
+        Me.btnGroupJoin.Name = "btnGroupJoin"
+        Me.btnGroupJoin.Size = New System.Drawing.Size(275, 40)
+        Me.btnGroupJoin.TabIndex = 2
+        Me.btnGroupJoin.Text = "Group join two tables"
+        Me.btnGroupJoin.UseVisualStyleBackColor = True
+        '
+        'btnJoinmultiple
+        '
+        Me.btnJoinmultiple.Font = New System.Drawing.Font("Georgia", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnJoinmultiple.Location = New System.Drawing.Point(927, 28)
+        Me.btnJoinmultiple.Name = "btnJoinmultiple"
+        Me.btnJoinmultiple.Size = New System.Drawing.Size(251, 40)
+        Me.btnJoinmultiple.TabIndex = 3
+        Me.btnJoinmultiple.Text = "Join multiple tables"
+        Me.btnJoinmultiple.UseVisualStyleBackColor = True
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Location = New System.Drawing.Point(415, 74)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.RowTemplate.Height = 24
+        Me.DataGridView2.Size = New System.Drawing.Size(372, 364)
+        Me.DataGridView2.TabIndex = 4
+        '
+        'DataGridView3
+        '
+        Me.DataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView3.Location = New System.Drawing.Point(811, 74)
+        Me.DataGridView3.Name = "DataGridView3"
+        Me.DataGridView3.RowTemplate.Height = 24
+        Me.DataGridView3.Size = New System.Drawing.Size(514, 364)
+        Me.DataGridView3.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(332, 50)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 17)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(736, 51)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 17)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(1224, 51)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 17)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Label3"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1341, 450)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DataGridView3)
+        Me.Controls.Add(Me.DataGridView2)
+        Me.Controls.Add(Me.btnJoinmultiple)
+        Me.Controls.Add(Me.btnGroupJoin)
+        Me.Controls.Add(Me.btnJoin2)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Shobaki-Final Project"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnJoin2 As Button
+    Friend WithEvents btnGroupJoin As Button
+    Friend WithEvents btnJoinmultiple As Button
+    Friend WithEvents DataGridView2 As DataGridView
+    Friend WithEvents DataGridView3 As DataGridView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ LinqToSQL/LinqToSQL/Form1.vb
@@ -0,0 +1,32 @@
+﻿
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim dbContext As New testDataContext
+        DataGridView1.DataSource = From orders In dbContext.Orders
+                                   Join OrderDetails In dbContext.Order_Details
+                                   On orders.OrderID Equals OrderDetails.OrderID
+                                   Join Product In dbContext.Products
+                                   On OrderDetails.ProductID Equals Product.ProductID
+                                   Where orders.CustomerID = "VINET"
+                                   Select orders.CustomerID, orders.OrderDate, OrderDetails.ProductID, OrderDetails.Quantity, Product.ProductName
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim dbContext As New testDataContext
+        Dim custOrd = From cust In dbContext.Customers
+                      Group Join Order In dbContext.Orders
+                                   On cust.CustomerID Equals Order.CustomerID
+                      Into CustOrders = Group
+                      From order In CustOrders.DefaultIfEmpty
+                      Where order.CustomerID Is Nothing
+                      Select New With {cust.CompanyName,
+                        cust.CustomerID}
+        DataGridView1.DataSource = custOrd.Distinct
+    End Sub
+End Class
